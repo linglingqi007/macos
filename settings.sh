4294c34737d9600691ba30b5c79e3c09c49e1cd0
凌凌漆 <if.true.else@gmail.com>
2024-11-03 17:00:54 +0800

    :bento: add lsd, procs, ripgreprc

--- settings.sh
+++ settings.sh
@@ -62,3 +62,34 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # security
 sudo spctl --master-disable # allow open all apps
+
+# 添加中文86五笔输入法
+defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict><key>InputSourceKind</key><string>TCIM</string><key>TISIntendedLanguage</key><string>zh-Hans</string><key>TISInputSourceID</key><string>com.apple.inputmethod.TCIM.WBX</string></dict>'
+
+# default handler
+duti -s com.todesktop.230313mzl4w4u92 .c all
+duti -s com.todesktop.230313mzl4w4u92 .cpp all
+duti -s com.todesktop.230313mzl4w4u92 .cs all
+duti -s com.todesktop.230313mzl4w4u92 .css all
+duti -s com.todesktop.230313mzl4w4u92 .js all
+duti -s com.todesktop.230313mzl4w4u92 .json all
+duti -s com.todesktop.230313mzl4w4u92 .jsx all
+duti -s com.todesktop.230313mzl4w4u92 .sass all
+duti -s com.todesktop.230313mzl4w4u92 .scss all
+duti -s com.todesktop.230313mzl4w4u92 .less all
+duti -s com.todesktop.230313mzl4w4u92 .vue all
+duti -s com.todesktop.230313mzl4w4u92 .go all
+duti -s com.todesktop.230313mzl4w4u92 .java all
+duti -s com.todesktop.230313mzl4w4u92 .lua all
+duti -s com.todesktop.230313mzl4w4u92 .md all
+duti -s com.todesktop.230313mzl4w4u92 .php all
+duti -s com.todesktop.230313mzl4w4u92 .pl all
+duti -s com.todesktop.230313mzl4w4u92 .py all
+duti -s com.todesktop.230313mzl4w4u92 .rb all
+duti -s com.todesktop.230313mzl4w4u92 .rs all
+duti -s com.todesktop.230313mzl4w4u92 .sh all
+duti -s com.todesktop.230313mzl4w4u92 .swift all
+duti -s com.todesktop.230313mzl4w4u92 .txt all
+duti -s com.todesktop.230313mzl4w4u92 .conf all
+duti -s com.todesktop.230313mzl4w4u92 .cfg all
+duti -s com.todesktop.230313mzl4w4u92 .ass all
